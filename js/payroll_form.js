142a5445f9659a33dd2c38db13553109ffd876ca
pallavikyama <kyamap98@gmail.com>
2020-11-23 12:41:09 +0530

    Modified: Specified startDate with day,month and year as select input item.

---
+++ js/payroll_form.js
@@ -0,0 +1,5 @@
+const salary = document.querySelector('#salary');
+const output = document.querySelector('.salary-output');
+salary.addEventListener('input', function() {
+    output.textContent = salary.value;
+});

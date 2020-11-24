79cf999137c3d3286cf78a13efb5e6f9304fefcf
pallavikyama <kyamap98@gmail.com>
2020-11-24 11:32:18 +0530

    Added: employeePayrollData object is populated on submitting the form.

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -1,6 +1,88 @@
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
-output.textContent = salary.value;
 salary.addEventListener('input', function() {
     output.textContent = salary.value;
 });
+
+class EmployeePayrollData {
+
+    // constructor
+    constructor(...params) {
+        this.name = params[0];
+        this.profileImage = params[1];
+        this.gender = params[2];
+        this.department = params[3];
+        this.salary = params[4];
+        this.startDate = params[5];
+        this.notes = params[6];
+    }
+
+    // getter and setter method
+
+    get name() { return this._name; }
+    set name(name) { this._name = name; }
+
+    get profileImage() { return this._profileImage; }
+    set profileImage(profileImage) { this._profileImage = profileImage; }
+
+    get gender() { return this._gender; }
+    set gender(gender) { this._gender = gender; }
+
+    get department() { return this._department; }
+    set department(department) { this._department = department; }
+
+    get salary() { return this._salary; }
+    set salary(salary) { this._salary = salary; }
+
+    get startDate() { return this._startDate; }
+    set startDate(startDate) { this._startDate = startDate; }
+
+    get notes() { return this._notes; }
+    set notes(notes) { this._notes = notes; }
+
+    // method
+    toString() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = !this.startDate ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
+        return `name=${this.name}, profile-image=${this.profileImage}, gender=${this.gender}, department=${this.department}, salary=${this.salary}, startDate=${empDate}, notes=${this.notes}`;
+    }
+}
+
+function save() {
+    let empName = document.querySelector('#name').value;
+    let imageNodeList = document.querySelectorAll('input[name="profile"]');
+    let empProfileImage;
+    for (let image of imageNodeList) {
+        if (image.checked) {
+            empProfileImage = image.value;
+            break;
+        }
+    }
+    let genderNodeList = document.querySelectorAll('input[name="gender"]');
+    let empGender;
+    for (let gender of genderNodeList) {
+        if (gender.checked) {
+            empGender = gender.value;
+            break;
+        }
+    }
+    let departmentNodeList = document.querySelectorAll('.checkbox:checked');
+    let empDepartment = new Array();
+    for (let department of departmentNodeList) {
+        if (department.checked) {
+            empDepartment.push(department.value);
+        }
+    }
+    let empSalary = document.querySelector('#salary').value;
+    let day = document.querySelector('#day').value;
+    let month = document.querySelector('#month').value;
+    let year = document.querySelector('#year').value;
+    let empStartDate = new Date(year, month, day);
+    let empNotes = document.querySelector('#notes').value;
+    try {
+        let employeePayrollData = new EmployeePayrollData(empName, empProfileImage, empGender, empDepartment, empSalary, empStartDate, empNotes);
+        console.log(employeePayrollData.toString());
+    } catch (e) {
+        console.error(e);
+    }
+}
